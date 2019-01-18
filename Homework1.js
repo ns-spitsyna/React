d3b4bf6f7435401174e3a8823c71bcb8bf819f4c
ns-spitsyna <45058971+ns-spitsyna@users.noreply.github.com>
2019-01-19 01:15:07 +0700

    Исправления ДЗ1
    Сцепка методов, использование own, перечень спонсоров.

--- Homework1.js
+++ Homework1.js
@@ -1,9 +1,7 @@
 let employers = ['Alex', '', 'ludmila', 'Viktor', '', 'oleg', 'iNna', 'Ivan', 'Alex', 'Olga', ' Ann'];
-let employersNames = employers.filter((name) =>{
-    return name.length > 0 && name.length != ''
-});
+let employersNames = employers.filter((name) =>{return name.length > 0 && name.length != ''}).map((item)=>item.toLowerCase().trim());
 
-employersNames= employersNames.map((item)=>item.toLowerCase().trim());
+//employersNames= employersNames.map((item)=>item.toLowerCase().trim());
 
 const sponsors = {
     cash: [40000, 5000, 30400, 12000],
@@ -14,20 +12,22 @@ const {cash, eu, rus} = sponsors;
 
 function calcCash(own = 0) {
      const everyCash = (accumulator, currentValue) => accumulator + currentValue;
-     let total = cash.reduce(everyCash);
+     let total = own + cash.reduce(everyCash);
      return total;   
 }
 
-let money = calcCash(cash);
+let money = calcCash(null, cash);
 
-function makeBusiness(owner, director, cash, emp) {
+function makeBusiness(owner, director ='Victor', cash, emp) {
 
-    let x = [...eu, ...rus];
-    let sumSponsors = `${x} unexpected sponsor `;
-   
+    //let x = [...eu, ...rus];
+    
+
+    let sumSponsors = [...eu, ...rus, 'unexpected sponsor'];
+    
 
     console.log(`We have a business. Owner: ${owner}, director: ${director}. Our budget:  ${cash}. And our employers: ${emp} \nAnd we have a sponsors:`);
-    console.log(...[sumSponsors]);
+    console.log(...sumSponsors);
     console.log(`Note. Be careful with  ${eu[0]}. It's a huge risk.`);
 }
-makeBusiness(...['Sam', 'Victor' , money, employersNames]);
+makeBusiness(...['Sam',  , money, employersNames]);
